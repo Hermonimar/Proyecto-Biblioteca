5adf789700180fcf5ea0b7de763a34987b03e0e2
laurabareno716 <lauram.barenob@uqvirtual.edu.co>
2024-09-29 01:44:09 -0500

    cambio7

--- Estudiante.java
+++ Estudiante.java
@@ -1,5 +1,6 @@
 
 
+
 import java.util.Collection;
 import java.util.LinkedList;
 
@@ -14,17 +15,35 @@ public class Estudiante extends Persona {
         prestamos = new LinkedList<>();
     }
 
+     /* 
+     * Metodo para obtener el estado
+     *  return estado
+     */
+
     public boolean isEstado() {
         return estado;
     }
+
+    /*
+     *  Metodo para cambiar el estado
+     */
 
     public void setEstado(boolean estado) {
         this.estado = estado;
     }
 
+     /* 
+     * Metodo para obtener el prestamo
+     *  return prestamos
+     */
+
     public Collection<Prestamo> getPrestamos() {
         return prestamos;
     }
+
+    /*
+     *  Metodo para cambiar los prestamos
+     */
 
     public void setPrestamos(Collection<Prestamo> prestamos) {
         this.prestamos = prestamos;
