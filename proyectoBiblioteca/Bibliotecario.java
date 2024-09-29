cc6cfab7c80de86fd0ca9b6a0ebb75131cb3808c
Monica Hernandez <monicam.hernandezv@uqvirtual.edu.co>
2024-09-29 01:42:56 -0500

    Cambio4

--- proyectoBiblioteca/Bibliotecario.java
+++ proyectoBiblioteca/Bibliotecario.java
@@ -7,48 +7,85 @@ import java.util.LinkedList;
 public class Bibliotecario extends Persona {
 
     private double salario;
-    private LocalDate fechaIngreso;
+    private int antiguedad;
     private Collection<Prestamo> prestamos;
 
-    public Bibliotecario(String nombre, String cedula, String correo, int telefono, double salario, LocalDate fechaIngreso){
+    public Bibliotecario(String nombre, String cedula, String correo, int telefono, double salario, int antiguedad){
         super(nombre, cedula, correo, telefono);
         this.salario=salario;
-        this.fechaIngreso=fechaIngreso;
+        this.antiguedad=antiguedad;
         prestamos = new LinkedList<>();
     }
 
+    /*
+     * Metodo para calcular el pago de los bibliotecarios
+     * return total + bonificacion
+     */
+
+    public double pagoBibliotecarios(int antiguedad) {
+        double total= 0.0;
+
+        for (Prestamo prestamo: prestamos) {
+            total += prestamo.getTotal() * 0.20; 
+        }
+
+        double bonificacion = total * (0.02 * antiguedad); 
+
+        return total + bonificacion; 
+    }
+
+     /*
+     * Metodo para obtener salario
+     * return codigo
+     */
     public double getSalario() {
         return salario;
     }
 
+    /*
+     * Metodo para modificar salario
+     * return codigo
+     */
     public void setSalario(double salario) {
         this.salario = salario;
     }
 
-    public LocalDate getFechaIngreso() {
-        return fechaIngreso;
+    /*
+     * Metodo para obtener antiguedad
+     * return codigo
+     */
+    public int getAntiguedad() {
+        return antiguedad;
     }
 
-    public void setFechaIngreso(LocalDate fechaIngreso) {
-        this.fechaIngreso = fechaIngreso;
+    /*
+     * Metodo para modificar antiguedad
+     * return codigo
+     */
+    public void setAntiguedad(int antiguedad) {
+        this.antiguedad = antiguedad;
     }
 
+     /*
+     * Metodo para obtener prestamos
+     * return codigo
+     */
     public Collection<Prestamo> getPrestamos() {
         return prestamos;
     }
 
+    /*
+     * Metodo para modificar prestamos
+     * return codigo
+     */
     public void setPrestamos(Collection<Prestamo> prestamos) {
         this.prestamos = prestamos;
     }
 
     @Override
     public String toString() {
-        return "Bibliotecario [salario=" + salario + ", fechaIngreso=" + fechaIngreso + ", prestamos=" + prestamos
-                + "]";
+        return "Bibliotecario [salario=" + salario + ", antiguedad=" + antiguedad + ", prestamos=" + prestamos + "]";
     }
-
-    
-
-    
+  
     
 }
